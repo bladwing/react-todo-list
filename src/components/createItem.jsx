d873f1758e78a5bcf30cf293ac39b1aed702c9b7
BLADWING <timoti.ponarenkO@gmail.com>
2022-06-12 02:32:33 +0400

    fix bug with displaying errors & solved warning

--- src/components/createItem.jsx
+++ src/components/createItem.jsx
@@ -5,6 +5,7 @@ class CreateItem extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      newItemInput: "",
       error: "",
     };
   }
@@ -15,12 +16,13 @@ class CreateItem extends React.Component {
         return (
          this.setState({ error: "გთხოვთ შეიყვანეთ დავალების სახელი..." })
         )
-      } else if (this.props.toDoItems.map(element => element.name).indexOf(this.refs.newItemInput.value) != -1) {
+      } else if (this.props.toDoItems.map(element => element.name).indexOf(this.refs.newItemInput.value) !== -1) {
         this.setState({ error: 'ესეთი დავალების სახელი უკვე არსებობს :('})
         this.refs.newItemInput.value = this.newItemInput.value;
         return;
       }
       this.props.createItem(this.refs.newItemInput.value);
+      this.setState({ error: "" })
       this.refs.newItemInput.value = '';
     }
     render() {
