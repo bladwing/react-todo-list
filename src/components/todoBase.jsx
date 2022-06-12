7e515d5921e13530c582210bb3c50cee5caf930e
BLADWING <timoti.ponarenkO@gmail.com>
2022-06-13 01:08:28 +0400

    change position for ID

--- src/components/todoBase.jsx
+++ src/components/todoBase.jsx
@@ -1,42 +1,42 @@
 const toDoItems = [
     {
-      name: 'შექმენი რეაქრ პროექტი',
       id: 0,
+      name: 'შექმენი რეაქრ პროექტი',
       completed: false
     },
     {
-      name: 'შექმენი დავალებების სია',
       id: 1,
+      name: 'შექმენი დავალებების სია',
       completetd: false
     },
     {
-      name: 'დაფუშე GitHub-ზე',
       id: 2,
+      name: 'დაფუშე GitHub-ზე',
       completed: false
     },
     {
-      name: "id - დამატება uuid - ით",
       id: 3,
+      name: "id - დამატება uuid - ით",
       completed: false
     },
     {
-      name: "შექმენი კომპონენტები",
       id: 4,
+      name: "შექმენი კომპონენტები",
       completed: false
     },
     {
-      name: "შექმენი კომპონენტების სტილები App.css ში.",
       id: 5,
+      name: "შექმენი კომპონენტების სტილები App.css ში.",
       completed: false
     },
     {
-      name: "შექმენი ღილაკები.",
       id: 6,
+      name: "შექმენი ღილაკები.",
       completed: true
     },
     {
-      name: "გამოაჩინე დავალებების სია",
       id: 7,
+      name: "გამოაჩინე დავალებების სია",
       completed: true
     }
    
